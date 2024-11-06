1be460600b87599772169a76420daf8c4087f10f
Rodolphe Quiedeville <r.quiedeville@smartway.ai>
2024-11-06 17:52:27 +0100

    Add test

--- test/sql/historize_init_test.sql
+++ test/sql/historize_init_test.sql
@@ -6,7 +6,7 @@ PREPARE init_histo AS
   SELECT historize_table_init('public','test_foobar');
 
 -- Define the number of tests to run
-SELECT plan(6);
+SELECT plan(7);
 
 CREATE TABLE test_foobar (id int) ;
 
@@ -25,6 +25,20 @@ SELECT has_column('public'::name, 'test_foobar'::name, 'histo_version',
 
 SELECT has_table('public'::name, 'test_foobar_log'::name, 'Table public.test_foobar_log exists');
 SELECT is_partitioned('public'::name, 'test_foobar_log'::name, 'Table public.test_foobar_log is partitioned');
+
+SELECT partitions_are(
+    'public', 'test_foobar_log',
+    ARRAY['test_foobar_log_' || TO_CHAR(NOW()::date, 'yyyymmdd'),
+          'test_foobar_log_' || TO_CHAR(NOW()::date + INTERVAL '1 day', 'yyyymmdd'),
+          'test_foobar_log_' || TO_CHAR(NOW()::date + INTERVAL '2 day', 'yyyymmdd'),
+          'test_foobar_log_' || TO_CHAR(NOW()::date + INTERVAL '3 day', 'yyyymmdd'),
+          'test_foobar_log_' || TO_CHAR(NOW()::date + INTERVAL '4 day', 'yyyymmdd'),
+          'test_foobar_log_' || TO_CHAR(NOW()::date + INTERVAL '5 day', 'yyyymmdd'),
+          'test_foobar_log_' || TO_CHAR(NOW()::date + INTERVAL '6 day', 'yyyymmdd')
+          ]
+);
+
+
 
 SELECT columns_are('public'::name, 'test_foobar_log'::name, ARRAY['id','eventtime','txid','data']);
 
--- test/sql/historize_start_test.sql
+++ test/sql/historize_start_test.sql
@@ -18,14 +18,16 @@ CREATE TABLE test_foobar (id int) ;
 
 SELECT results_eq('init_histo',  ARRAY[0], 'init is successful and return 0');
 
-
-
 SELECT results_eq('start_histo',  ARRAY[0], 'start is successful and return 0');
 
 -- Check we have all wanted objects created
 --
 --
+SELECT has_function('test_foobar_historization_update_trg');
+SELECT has_function('test_foobar_historization_insert_trg');
 
+SELECT has_trigger('public'::name, 'test_foobar'::name, 'test_foobar_historization_insert_trg'::name);
+SELECT has_trigger('public'::name, 'test_foobar'::name, 'test_foobar_historization_update_trg'::name);
 
 
 SELECT * FROM finish();
