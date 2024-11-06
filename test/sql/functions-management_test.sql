6dc55b28b081acbb7049f092ca74d6077d0aa024
Rodolphe Quiedeville <r.quiedeville@smartway.ai>
2024-11-06 15:30:47 +0100

    Fix unit tests on functions-management_test.sql

--- test/sql/functions-management_test.sql
+++ test/sql/functions-management_test.sql
@@ -1,41 +1,43 @@
 BEGIN;
 
+DEALLOCATE PREPARE ALL;
+
 -- six tests will be run
 SELECT plan(3);
 
 
-CREATE TABLE test_foobar (id int) PARTITION BY RANGE(id);
+CREATE TABLE test_foobar_log (id int) PARTITION BY RANGE(id);
 
 
 PREPARE call_func AS
-SELECT create_tab_part(
+SELECT historize_create_partition(
   'test_foobar',
   1 - EXTRACT(DAY FROM now() - '2024-01-01')::int
 ) ;
 
 SELECT results_eq(
        'call_func',
-       ARRAY[1]
+       ARRAY[0]
 );
 
-SELECT create_tab_part(
+SELECT historize_create_partition(
   'test_foobar',
   1 - EXTRACT(DAY FROM now() - '2024-01-02')::int
 ) ;
 
 SELECT partitions_are(
-    'public', 'test_foobar',
-    ARRAY[ 'test_foobar_20240102', 'test_foobar_20240103' ]
+    'public', 'test_foobar_log',
+    ARRAY[ 'test_foobar_log_20240102', 'test_foobar_log_20240103' ]
 );
 
-SELECT drop_tab_part(
+SELECT historize_drop_partition(
   'test_foobar',
   1 - EXTRACT(DAY FROM now() - '2024-01-02')::int
 ) ;
 
 SELECT partitions_are(
-    'public', 'test_foobar',
-    ARRAY[ 'test_foobar_20240102' ]
+    'public', 'test_foobar_log',
+    ARRAY[ 'test_foobar_log_20240102' ]
 );
 
 SELECT * FROM finish();
