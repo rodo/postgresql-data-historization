91ec07dc25158582cada6f07ea23ddb72fa3cfd3
Rodolphe Quiedeville <r.quiedeville@smartway.ai>
2024-12-10 15:41:11 +0100

    Add nex function historize_table_clean

--- pgtle.data_historization-1.0.0.sql
+++ pgtle.data_historization-1.0.0.sql
@@ -168,15 +168,19 @@ $$
 DECLARE
     partition varchar;
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema=schema_source AND table_name=table_source) THEN
-      RAISE EXCEPTION 'table %.% does not exists', schema_source, table_source USING HINT = 'Check the table name and schema, fix the search_path is case of needed', ERRCODE = '42P01';
+    IF NOT EXISTS (SELECT 1 FROM information_schema.tables
+                   WHERE table_schema=schema_source
+                   AND table_name=table_source)
+    THEN
+      RAISE EXCEPTION 'table %.% does not exists', schema_source, table_source
+        USING HINT = 'Check the table name and schema, fix the search_path is case of needed',
+        ERRCODE = '42P01';
     END IF;
 
-    -- Reset the historization to ensure we can drop the table at no risk
-    EXECUTE format('
-       SELECT historize_table_reset(%L, %L)', schema_source, table_source );
+    -- Stop the historization to ensure to not block users doing an error
+    PERFORM historize_table_stop(schema_source, table_source);
 
-    EXECUTE format('DROP TABLE %',  historize_get_logname(schema_dest, table_source));
+    EXECUTE format('DROP TABLE %s ',  historize_get_logname(schema_source, table_source ));
 
 END;
 $$;
@@ -189,7 +193,7 @@ CREATE OR REPLACE FUNCTION historize_table_clean(table_source NAME)
     LANGUAGE plpgsql AS
 $$
 BEGIN
-    PERFORM historize_table_reset('public'::name, table_source);
+    PERFORM historize_table_clean('public'::name, table_source);
 END;
 $$;
 -- This function is used to define conr entries in another database
--- test/sql/functions-tools_test.sql
+++ test/sql/functions-tools_test.sql
@@ -5,7 +5,7 @@ SELECT plan(2);
 
 SELECT has_function('historize_get_logname'::name);
 
-SELECT is(historize_get_logname('fi', 'foo'), 'fi.foo_log'::text, 'fi.foo_log' );
+SELECT is(historize_get_logname('fi', 'foo'), 'fi.foo_log'::text, 'fi.foo_log');
 
 SELECT * FROM finish();
 -- Always end unittest with a rollback
